cef6ef80941b620e337c6c08dd5789d791f3cb4d
Daniel <sekd01@uqo.ca>
2025-11-26 20:50:26 -0500

    Ajout des filtres pour la page des produits et mise-en-page. Scripts de modification ajoutés également pour couvrir les portions vides dans les tables.

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
         const { q } = req.query;
         const request = new sql.Request();
 
-        request.timeout = 60000;
+        request.requestTimeout = 60000;
 
         let where = '1 = 1';
 
@@ -33,7 +33,328 @@ router.get('/', async (req, res) => {
         console.error('GET /api/products error:', err);
         res.status(500).json({ error: 'Erreur serveur', detail: err.message });
     }
-    
+});
+
+//GET API pour les filtres
+router.get('/filter', async (req, res) => {
+    const {
+        category,
+        minPrice,
+        maxPrice,
+        //Filtres pour CPU
+        corecount,
+        coreclock,
+        boostclock,
+        microarchitecture,
+        tdp,
+        graphics,
+        //Filtres pour GPU
+        memory,
+        mcoreclock,
+        mboostclock,
+        length,
+        //Filtres pour moniteurs
+        screensize,
+        resolution,
+        refreshrate,
+        responsetime,
+        paneltype,
+        aspectratio,
+        //Filtres pour ram
+        speed,
+        modules,
+        firstwordlat,
+        caslat,
+        //Filtres pour hard drives
+        hddtype,
+        interface,
+        capacity,
+        //Filtres pour alimentation
+        pstype,
+        efficiency,
+        wattage,
+        modular,
+        //Filtres pour boîtiers
+        casetype,
+        sidepanel,
+        externalvol,
+        internal35bays,
+        //Filtres pour cartes-mère
+        socket,
+        formfactor,
+        maxmemory,
+        memoryslots
+    } = req.query
+
+    try {
+        const request = new sql.Request();
+        request.timeout = 60000;
+
+        let joins = 'LEFT JOIN Categories c ON p.category_id = c.category_id';
+        let where = '1 = 1';
+        //Filtre pour les catégories
+        if (category) {
+            where += ' AND c.category_name = @category';
+            request.input('category', sql.NVarChar, category);
+            }
+        //Filtres généraux
+        if (minPrice) {
+            where += ' AND p.product_price >= @minPrice';
+            request.input('minPrice', sql.Decimal(10, 2), minPrice);
+            }
+
+        if (maxPrice) {
+            where += ' AND p.product_price <= @maxPrice';
+            request.input('maxPrice', sql.Decimal(10, 2), maxPrice);
+            }
+
+        //Filtres spécifiques
+        //Filtre CPU
+        let hasCPU = false;
+        
+        if (corecount) {
+            hasCPU = true;
+            where += ' AND cs.core_count = @corecount';
+            request.input('corecount', sql.Int, corecount);
+        }
+        if (coreclock) {
+            hasCPU = true;
+            where +=' AND cs.core_clock_ghz = @coreclock';
+            request.input('coreclock', sql.Decimal(6, 3), coreclock);
+        }
+        if (boostclock) {
+            hasCPU = true;
+            where +=' AND cs.boost_clock_ghz = @boostclock';
+            request.input('corecount', sql.Decimal(6, 3), boostclock);
+        }
+        if (microarchitecture) {
+            hasCPU = true;
+            where +=' AND cs.microarchitecture = @microarchitecture';
+            request.input('microarchitecture', sql.NVarChar, microarchitecture);
+        }
+        if (tdp) {
+            hasCPU = true;
+            where +=' AND cs.tdp_watts = @tdp';
+            request.input('tdp', sql.Decimal, tdp);
+        }
+        if (graphics) {
+            hasCPU = true;
+            where +=' AND cs.graphics = @graphics';
+            request.input('graphics', sql.NVarChar, graphics);
+        }
+        if (hasCPU) {
+            joins += ' JOIN CPU_Specs cs ON p.product_id = cs.product_id';
+        }
+
+        //Filtre GPU
+        let hasGPU = false;
+
+        if (memory) {
+            hasGPU = true;
+            where += ' AND gs.memory = @memory';
+            request.input('memory', sql.Int, memory);
+        }
+        if (mcoreclock) {
+            hasGPU = true;
+            where +=' AND gs.core_clock = @mcoreclock';
+            request.input('mcoreclock', sql.Int, mcoreclock);
+        }
+        if (mboostclock) {
+            hasGPU = true;
+            where += ' AND gs.boost_clock = @mboostclock';
+            request.input('mboostclock', sql.Int, mboostclock);
+        }
+        if (length) {
+            hasGPU = true;
+            where += ' AND gs.length = @length';
+            request.input('length', sql.Int, length);
+        }
+        if (hasGPU) {
+            joins += ' JOIN GPU_Specs gs ON p.product_id = gs.product_id';
+        }
+
+        //Filtre moniteurs
+        let hasMon = false
+
+        if (screensize) {
+            hasMon = true;
+            where += ' AND ms.screen_size = @screensize';
+            request.input('screensize', sql.Int, screensize);
+        }
+        if (resolution) {
+            hasMon = true;
+            where += ' AND ms.resolution = @resolution';
+            request.input('resolution', sql.NVarChar, resolution);
+        }
+        if (refreshrate) {
+            hasMon = true;
+            where += 'AND ms.refresh_rate = @refreshrate';
+            request.input('refreshrate', sql.Int, refreshrate);
+        }
+        if (responsetime) {
+            hasMon = true;
+            where += ' AND ms.response_time = @responsetime';
+            request.input('responsetime', sql.Decimal(5, 2), responsetime);
+        }
+        if (paneltype) {
+            hasMon = true;
+            where += ' AND ms.panel_type = @paneltype';
+            request.input('paneltype', sql.NVarChar, paneltype);
+        }
+        if (aspectratio) {
+            hasMon = true;
+            where += ' AND ms.aspect_ratio = @aspectratio';
+            request.input('aspectratio', sql.Int, aspectratio);
+        }
+        if (hasMon) {
+            joins += ' JOIN Monitor_Specs ms ON p.product_id = ms.product_id';
+        }
+
+        //Filtre Memory/RAM
+        let hasMRAM = false;
+
+        if (speed) {
+            hasMRAM = true;
+            where +=' AND mr.speed = @speed';
+            request.input('speed', sql.Int, speed);
+        }
+        if (modules) {
+            hasMRAM = true;
+            where +=' AND mr.modules = @modules';
+            request.input('modules', sql.NVarChar, modules);
+        }
+        if (firstwordlat) {
+            hasMRAM = true;
+            where +=' AND mr.first_word_latency = @firstwordlat';
+            request.input('firstwordlat', sql.Int, firstwordlat);
+        }
+        if (caslat) {
+            hasMRAM = true;
+            where += ' AND mr.cas_latency = @caslat';
+            request.input('caslat', sql.Int, caslat);
+        }
+        if (hasMRAM) {
+            joins += ' JOIN Memory_Specs mr ON p.product_id = mr.product_id';
+        }
+
+        //Filtre Hard drives
+        let hasHDD = false;
+
+        if (hddtype) {
+            hasHDD = true;
+            where += ' AND hs.type = @hddtype';
+            request.input('hddtype', sql.NVarChar, hddtype);
+        }
+        if (interface) {
+            hasHDD = true;
+            where += ' AND hs.interface = @interface';
+            request.input('interface', sql.NVarChar, interface);
+        }
+        if (capacity) {
+            hasHDD = true;
+            where += 'AND hs.capacity = @capacity';
+            request.input('capacity', sql.Int, capacity);
+        }
+        if (hasHDD){
+            joins += ' JOIN Hard_Drive_Specs hs ON p.product_id = hs.product_id';
+        }
+
+        //Filtre alimentations
+        let hasPS = false;
+
+        if (pstype) {
+            hasPS = true;
+            where += ' AND ps.type = @pstype';
+            request.input('pstype', sql.NVarChar, pstype);
+        }
+        if (efficiency) {
+            hasPS = true;
+            where += ' AND ps.efficiency = @efficiency';
+            request.input('efficiency', sql.NVarChar, efficiency);
+        }
+        if (wattage) {
+            hasPS = true;
+            where += ' AND ps.wattage = @wattage';
+            request.input('wattage', sql.Int, wattage);
+        }
+        if (modular) {
+            hasPS = true;
+            where += ' AND ps.modular = @modular';
+            request.input('modular', sql.NVarChar, modular);
+        }
+        if (hasPS) {
+            joins += ' JOIN Power_supply_Specs ps ON p.product_id = ps.product_id';
+        }
+
+        //Filtre boîtiers
+        let hasCase = false;
+
+        if (casetype) {
+            hasCase = true;
+            where += ' AND ca.type = @type';
+            request.input('casetype', sql.NVarChar, casetype);
+        }
+        if (sidepanel) {
+            hasCase = true;
+            where += ' AND ca.side_panel = @sidepanel';
+            request.input('sidepanel', sql.NVarChar, sidepanel);
+        }
+        if (externalvol) {
+            hasCase = true;
+            where += ' AND ca.external_volume = @externalvol';
+            request.input('externalvol', sql.Decimal(4, 2), externalvol);
+        }
+        if (internal35bays) {
+            hasCase = true;
+            where += ' AND ca.internal_35_bays = @internal35bays';
+            request.input('internal35bays', sql.Int, internal35bays);
+        }
+        if (hasCase) {
+            joins += ' JOIN Case_Specs ca ON p.product_id = ca.product_id';
+        }
+
+        //Filtre carte-mère
+        let hasMB = false;
+
+        if (socket) {
+            hasMB = true;
+            where += ' AND mb.socket = @socket';
+            request.input('socket', sql.NVarChar, casetype);
+        }
+        if (formfactor) {
+            hasMB = true;
+            where += ' AND mb.form_factor = @formfactor';
+            request.input('formfactor', sql.NVarChar, formfactor);
+        }
+        if (maxmemory) {
+            hasMB = true;
+            where += ' AND mb.max_memory = @maxmemory';
+            request.input('maxmemory', sql.Int, maxmemory);
+        }
+        if (memoryslots) {
+            hasMB = true;
+            where += ' AND mb.memory_slots = @memoryslots';
+            request.input('memoryslots', sql.NVarChar, memoryslots);
+        }
+        if (hasMB) {
+            joins += ' JOIN Motherboard_Specs mb ON p.product_id = mb.product_id';
+        }
+
+        //query final
+        const query = `
+            SELECT p.*, c.category_name
+            FROM Products p
+            ${joins}
+            WHERE ${where}
+            ORDER BY p.product_name;
+        `;
+
+        const result = await request.query(query);
+        res.json(result.recordset);
+    } catch (err){
+        console.error("GET /api/products/filter error:", err);
+        res.status(500).json({ error: "Erreur serveur", detail: err.message });
+    }
 });
 //GET API pour les produits et leurs spécifications
 router.get('/:id', async (req, res) => {
@@ -129,5 +450,6 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ error: 'Erreur serveur', detail: err.message });
 }
 });
+
 
 module.exports = router;
--- docker/sql/PC4U_database_alter.sql
+++ docker/sql/PC4U_database_alter.sql
@@ -1,56 +1,325 @@
-ALTER TABLE CPU_Specs
-ALTER COLUMN boost_clock_ghz DECIMAL(6,3) NULL;
+/* remplacer des nulls encombrants dans certaines tables */
+UPDATE Monitor_Specs
+SET screen_size =
+	CASE
+		WHEN resolution LIKE '1360x768' THEN 13
+		WHEN resolution LIKE '1366x768' THEN 13
+		WHEN resolution LIKE '1440x900' THEN 14
+		WHEN resolution LIKE '1600x900' THEN 19
+		WHEN resolution LIKE '1920x1080' THEN 22
+		WHEN resolution LIKE '1920x1200' THEN 24
+		WHEN resolution LIKE '2560x1080' THEN 29
+		WHEN resolution LIKE '2560x1440' THEN 27
+		WHEN resolution LIKE '2560x1600' THEN 28
+		WHEN resolution LIKE '2560x2880' THEN 27
+		WHEN resolution LIKE '3440x1440' THEN 34
+		WHEN resolution LIKE '3840x1600' THEN 38
+		WHEN resolution LIKE '3840x2160' THEN 27
+		WHEN resolution LIKE '3840x2560' THEN 32
+		WHEN resolution LIKE '5120x1440' THEN 49
+		WHEN resolution LIKE '5120x2160' THEN 34
+		WHEN resolution LIKE '7680x4320' THEN 32
+		ELSE screen_size
+	END
+WHERE screen_size IS NULL;
 
-ALTER TABLE CPU_Specs
-ALTER COLUMN graphics NVARCHAR(200) NULL;
+UPDATE Monitor_Specs
+SET refresh_rate = 60
+WHERE refresh_rate = 0;
 
-ALTER TABLE GPU_Specs
-ALTER COLUMN core_clock INT NULL;
 
-ALTER TABLE GPU_Specs
-ALTER COLUMN boost_clock INT NULL;
+UPDATE Monitor_Specs
+SET response_time =
+    CASE
+		WHEN panel_type = 'IPS' AND refresh_rate <= 75 THEN 5.00
+        WHEN panel_type = 'IPS' AND refresh_rate BETWEEN 80 AND 170 THEN 4.00
+        WHEN panel_type = 'IPS' AND refresh_rate >= 180 THEN 1.00
+        WHEN panel_type = 'VA' AND refresh_rate <= 75 THEN 8.00
+        WHEN panel_type = 'VA' AND refresh_rate BETWEEN 80 AND 170 THEN 6.00
+        WHEN panel_type = 'VA' AND refresh_rate = 180 THEN 4.00
+        WHEN panel_type = 'VA' AND refresh_rate >= 240 THEN 3.00
+        WHEN panel_type = 'TN' THEN 0.50
+        WHEN panel_type LIKE '%Mini%' AND refresh_rate <= 120 THEN 4.00
+        WHEN panel_type LIKE '%Mini%' AND refresh_rate > 120 THEN 2.00
+        WHEN panel_type LIKE '%OLED%' THEN 0.03
+        ELSE 5.00
+    END
+WHERE response_time IS NULL;
 
-ALTER TABLE GPU_Specs
-ALTER COLUMN length INT NULL;
+UPDATE Memory_Specs
+SET modules =
+    CASE
+        WHEN modules = '8,4'   THEN '8x4'
+        WHEN modules = '8,24'  THEN '8x24'
+        WHEN modules = '8,8'   THEN '8x8'
+        WHEN modules = '4,24'  THEN '4x24'
+        WHEN modules = '2,1'   THEN '2x1'
+        WHEN modules = '4,8'   THEN '4x8'
+        WHEN modules = '2,8'   THEN '2x8'
+        WHEN modules = '3,2'   THEN '3x2'
+        WHEN modules = '1,4'   THEN '1x4'
+        WHEN modules = '4,64'  THEN '4x64'
+        WHEN modules = '1,2'   THEN '1x2'
+        WHEN modules = '1,32'  THEN '1x32'
+        WHEN modules = '4,32'  THEN '4x32'
+        WHEN modules = '2,48'  THEN '2x48'
+        WHEN modules = '1,8'   THEN '1x8'
 
-ALTER TABLE Monitor_Specs
-ALTER COLUMN refresh_rate INT NULL;
+        WHEN modules = '4,4'   THEN '4x4'
+        WHEN modules = '2,16'  THEN '2x16'
+        WHEN modules = '2,64'  THEN '2x64'
+        WHEN modules = '4,2'   THEN '4x2'
+        WHEN modules = '8,48'  THEN '8x48'
+        WHEN modules = '2,32'  THEN '2x32'
+        WHEN modules = '8,64'  THEN '8x64'
+        WHEN modules = '1,64'  THEN '1x64'
+        WHEN modules = '4,48'  THEN '4x48'
+        WHEN modules = '1,16'  THEN '1x16'
+        WHEN modules = '8,32'  THEN '8x32'
+        WHEN modules = '3,4'   THEN '3x4'
+        WHEN modules = '8,16'  THEN '8x16'
+        WHEN modules = '1,48'  THEN '1x48'
+        WHEN modules = '3,8'   THEN '3x8'
+        WHEN modules = '3,1'   THEN '3x1'
+        WHEN modules = '2,4'   THEN '2x4'
+        WHEN modules = '2,2'   THEN '2x2'
+        WHEN modules = '3,16'  THEN '3x16'
+        WHEN modules = '2,24'  THEN '2x24'
+        WHEN modules = '4,16'  THEN '4x16'
+        WHEN modules = '1,1'   THEN '1x1'
+        WHEN modules = '1,24'  THEN '1x24'
 
-ALTER TABLE Monitor_Specs
-ALTER COLUMN response_time INT NULL;
+        ELSE modules
+    END;
 
-ALTER TABLE Monitor_Specs
-ALTER COLUMN panel_type NVARCHAR(50) NULL;
+UPDATE Memory_Specs
+SET first_word_latency = 10
+WHERE first_word_latency IS NULL;
 
-ALTER TABLE Memory_Specs
-ALTER COLUMN price_per_gb DECIMAL(10,2) NULL;
+UPDATE Hard_Drive_Specs
+SET interface = 'USB Type-A 3.2 Gen 1'
+WHERE interface IS NULL
+   OR LTRIM(RTRIM(interface)) = '';
 
-ALTER TABLE Memory_Specs
-ALTER COLUMN color NVARCHAR(50) NULL;
+UPDATE Power_supply_Specs
+SET efficiency =
+    CASE
+        WHEN type = 'ATX' AND modular = 'Full' AND wattage >= 1200 THEN 'Platinum'
+        WHEN type = 'ATX' AND modular = 'Full' AND wattage >= 850 THEN 'Gold'
+        WHEN type = 'ATX' AND modular = 'Full' THEN 'Gold'
 
-ALTER TABLE Hard_Drive_Specs
-ALTER COLUMN interface NVARCHAR(400) NULL;
+        WHEN type = 'ATX' AND modular = 'Semi' THEN 'Bronze'
+        WHEN type = 'ATX' AND modular = 'FALSE' THEN 'Bronze'
 
-ALTER TABLE Hard_Drive_Specs
-ALTER COLUMN price_per_gb DECIMAL(10,2) NULL;
+        WHEN type IN ('SFX', 'TFX') THEN 'Bronze'
 
-ALTER TABLE Hard_Drive_Specs
-ALTER COLUMN color NVARCHAR(50) NULL;
+        ELSE 'Bronze'   -- fallback safe
+    END
+WHERE LTRIM(RTRIM(efficiency)) = '';
 
-ALTER TABLE Power_supply_Specs
-ALTER COLUMN efficiency NVARCHAR(50) NULL;
-
-ALTER TABLE Power_supply_Specs
-ALTER COLUMN color NVARCHAR(50) NULL;
+UPDATE Case_Specs
+SET side_panel = 'SECC'
+WHERE side_panel = ''
 
 ALTER TABLE Case_Specs
-ALTER COLUMN color NVARCHAR(50) NULL;
+ALTER column external_volume DECIMAL(5,2)
 
-ALTER TABLE Case_Specs
-ALTER COLUMN side_panel NVARCHAR(100) NULL;
+UPDATE Case_Specs
+SET external_volume = (470*206*482)/1000000
+WHERE product_id = 6681
 
-ALTER TABLE Case_Specs
-ALTER COLUMN external_volume DECIMAL(4,2) NULL;
+UPDATE Case_Specs
+SET external_volume = (620*235*595)/1000000
+WHERE product_id = 6692
 
-ALTER TABLE Motherboard_Specs
-ALTER COLUMN color NVARCHAR(50) NULL;
+UPDATE Case_Specs
+SET external_volume = (469*285*400)/1000000
+WHERE product_id = 6697
+
+UPDATE Case_Specs
+SET external_volume = (467*220*486)/1000000
+WHERE product_id = 6712
+
+UPDATE Case_Specs
+SET external_volume = (351*200*390)/1000000
+WHERE product_id = 6721
+
+UPDATE Case_Specs
+SET external_volume = (477*220*486)/1000000
+WHERE product_id = 6723
+
+UPDATE Case_Specs
+SET external_volume = (405*205*400)/1000000
+WHERE product_id = 6732
+
+UPDATE Case_Specs
+SET external_volume = (412*185*440)/1000000
+WHERE product_id = 6733
+
+UPDATE Case_Specs
+SET external_volume = 39.64
+WHERE product_id = 6734
+
+UPDATE Case_Specs
+SET external_volume = 58.05
+WHERE product_id = 6737
+
+UPDATE Case_Specs
+SET external_volume = (520*205*500)/1000000
+WHERE product_id = 6773
+
+UPDATE Case_Specs
+SET external_volume = (350*350*350)/1000000
+WHERE product_id = 6774
+
+UPDATE Case_Specs
+SET external_volume = (558*490*490)/3000000
+WHERE product_id = 6781
+
+UPDATE Case_Specs
+SET external_volume = (435*368*368)/3000000
+WHERE product_id = 6782
+
+UPDATE Case_Specs
+SET external_volume = (610*481*529)/1000000
+WHERE product_id = 6783
+
+UPDATE Case_Specs
+SET external_volume = (553*245*484)/1000000
+WHERE product_id = 6818
+
+UPDATE Case_Specs
+SET external_volume = (40.5*20.5*49.2)/1000
+WHERE product_id = 6825
+
+UPDATE Case_Specs
+SET external_volume = (650*306*651)/1000000
+WHERE product_id = 6843
+
+UPDATE Case_Specs
+SET external_volume = (516*224*510)/1000000
+WHERE product_id = 6851
+
+UPDATE Case_Specs
+SET external_volume = (516*224*510)/1000000
+WHERE product_id = 6852
+
+UPDATE Case_Specs
+SET external_volume = (556*279*540)/1000000
+WHERE product_id = 6854
+
+UPDATE Case_Specs
+SET external_volume = 40.3
+WHERE product_id = 6857
+
+UPDATE Case_Specs
+SET external_volume = (435.5*217.5*410)/1000000
+WHERE product_id = 6858
+
+UPDATE Case_Specs
+SET external_volume = (496*217*469)/1000000
+WHERE product_id = 6861
+
+UPDATE Case_Specs
+SET external_volume = (278*216*294)/1000000
+WHERE product_id = 6872
+
+UPDATE Case_Specs
+SET external_volume = (525*228*502)/1000000
+WHERE product_id = 6873
+
+UPDATE Case_Specs
+SET external_volume = (702*306*410)/1000000
+WHERE product_id = 6878
+
+UPDATE Case_Specs
+SET external_volume = 33.9
+WHERE product_id = 6887
+
+UPDATE Case_Specs
+SET external_volume = 38.24
+WHERE product_id = 6889
+
+UPDATE Case_Specs
+SET external_volume = 38.24
+WHERE product_id = 6890
+
+UPDATE Case_Specs
+SET external_volume = (307*698*698)/1000000
+WHERE product_id = 6919
+
+UPDATE Case_Specs
+SET external_volume = (398*276*351)/1000000
+WHERE product_id = 6920
+
+UPDATE Case_Specs
+SET external_volume = (210*495*463)/1000000
+WHERE product_id = 6949
+
+UPDATE Case_Specs
+SET external_volume = (43*20.5*47)/1000
+WHERE product_id = 6962
+
+UPDATE Case_Specs
+SET external_volume = (420*212*403)/1000000
+WHERE product_id = 6964
+
+UPDATE Case_Specs
+SET external_volume = (420*212*403)/1000000
+WHERE product_id = 6965
+
+UPDATE Case_Specs
+SET external_volume = (393*230*460)/1000000
+WHERE product_id = 7051
+
+UPDATE Case_Specs
+SET external_volume = (397*210*425)/1000000
+WHERE product_id = 7056
+
+UPDATE Case_Specs
+SET external_volume = (363*210*447)/1000000
+WHERE product_id = 7062
+
+UPDATE Case_Specs
+SET external_volume = (490*192*490)/1000000
+WHERE product_id = 7118
+
+UPDATE Case_Specs
+SET external_volume = (35.8*20.3*41.9)/1000
+WHERE product_id = 7126
+
+UPDATE Case_Specs
+SET external_volume = 13.28
+WHERE product_id = 7129
+
+UPDATE Case_Specs
+SET external_volume = (575.5*558.5*234)/1000000
+WHERE product_id = 7191
+
+UPDATE Case_Specs
+SET external_volume = (575.5*558.5*234)/1000000
+WHERE product_id = 7192
+
+UPDATE Case_Specs
+SET external_volume = (678.5*279*674)/1000000
+WHERE product_id = 7196
+
+UPDATE Case_Specs
+SET external_volume = (370*210*480)/1000000
+WHERE product_id = 7229
+
+UPDATE Case_Specs
+SET external_volume = (370*210*480)/1000000
+WHERE product_id = 7230
+
+UPDATE Case_Specs
+SET external_volume = (421*210*499)/1000000
+WHERE product_id = 7237
+
+UPDATE Case_Specs
+SET external_volume = (430*215*450)/1000000
+WHERE product_id = 7250
+
+UPDATE Case_Specs
+SET external_volume = (430*215*450)/1000000
+WHERE product_id = 7251
