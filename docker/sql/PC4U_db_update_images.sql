107b86146c35ad474b779a025583e39d2305da96
Daniel <sekd01@uqo.ca>
2025-12-09 04:49:03 -0500

    Ajout des images pour les produits
    Le backend filtre désormais uniquement les produits ayant un image_url non null pour l'affichage

---
+++ docker/sql/PC4U_db_update_images.sql
@@ -0,0 +1,247 @@
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Xeon.jpg'
+WHERE product_name LIKE '%Intel%' AND product_name LIKE '%Xeon%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Celeron.jpg'
+WHERE product_name LIKE '%Intel%' AND product_name LIKE '%Celeron%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_i9_12.jpg'
+WHERE product_name LIKE '%Intel Core%' AND product_name LIKE '%i9-12%' /* à vérifier */
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Pentium.jpg'
+WHERE product_name LIKE '%Intel%' AND product_name LIKE '%Pentium%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_i5.jpg'
+WHERE product_name LIKE '%Intel Core%' AND product_name LIKE '%i5%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD A-series.jpg'
+WHERE product_name LIKE '%AMD A%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Athlon.jpg'
+WHERE product_name LIKE '%AMD Athlon%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_EPYC.jpg'
+WHERE product_name LIKE '%AMD EPYC%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_FX.jpg'
+WHERE product_name LIKE '%AMD FX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Opteron.png'
+WHERE product_name LIKE '%AMD Opteron%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Phenom_II_X4.png'
+WHERE product_name LIKE '%AMD Phenom II X4%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Phenom_II_X6.webp'
+WHERE product_name LIKE '%AMD Phenom II X6%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Ryzen_3.jpg'
+WHERE product_name LIKE '%AMD Ryzen 3%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Ryzen_5.jpg'
+WHERE product_name LIKE '%AMD Ryzen 5%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Ryzen_7.jpg'
+WHERE product_name LIKE '%AMD Ryzen 7%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Ryzen_9.jpg'
+WHERE product_name LIKE '%AMD Ryzen 9%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/AMD_Ryzen_Threadripper.jpg'
+WHERE product_name LIKE '%AMD Ryzen Threadripper%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_2.jpg'
+WHERE product_name LIKE '%Intel Core 2%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_i3.jpg'
+WHERE product_name LIKE '%Intel Core i3%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_i7.jpg'
+WHERE product_name LIKE '%Intel Core i7%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_i9_X.jpg'
+WHERE product_name LIKE '%Intel Core i9%' AND product_name LIKE '%X%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_Ultra.jpg'
+WHERE product_name LIKE '%Intel Core Ultra%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_Core_Ultra.jpg'
+WHERE product_name LIKE '%Intel Core Ultra%'
+
+UPDATE Products
+SET image_url = './assets/img/products/CPU/Intel_300.jpg'
+WHERE product_name LIKE '%Intel 300%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX2060.png'
+WHERE product_name LIKE '%Asus DUAL EVO GeForce RTX 2060 12GB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX4060.jpg'
+WHERE product_name LIKE '%Asus DUAL EVO OC GeForce RTX 4060%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX4060.jpg'
+WHERE product_name LIKE '%Asus DUAL EVO OC GeForce RTX 4060%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX4070.png'
+WHERE product_name LIKE '%Asus DUAL EVO OC GeForce RTX 4070%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_GTX.jpg'
+WHERE product_name LIKE '%Asus DUAL GeForce GTX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX4070.png'
+WHERE product_name LIKE '%Asus DUAL GeForce RTX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_Dual_RTX3060.jpg'
+WHERE product_name LIKE '%Asus Dual GeForce RTX 3060 V2 OC Edition GeForce RTX 3060 12GB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_GT.png'
+WHERE product_name LIKE '%Asus GT%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_STRIX_GAMING.webp'
+WHERE product_name LIKE '%Asus STRIX GAMING%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_STRIX_GAMING.webp'
+WHERE product_name LIKE '%Asus STRIX GAMING%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_ROG_STRIX_GAMING.jpg'
+WHERE product_name LIKE '%Asus ROG STRIX GAMING%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_ROG_STRIX_WHITE.png'
+WHERE product_name LIKE '%Asus ROG STRIX WHITE OC GeForce RTX 3080 10GB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_TUF_Gaming.png'
+WHERE product_name LIKE '%Asus TUF GAMING GeForce RTX 4%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Asus_TUF_Gaming_OC.png'
+WHERE product_name LIKE '%Asus TUF GAMING OC%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/EVGA_0.jpg'
+WHERE product_name LIKE '%EVGA 0%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/EVGA_ACX.webp'
+WHERE product_name LIKE '%EVGA ACX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/EVGA ACX.webp'
+WHERE product_name LIKE '%EVGA ACX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/EVGA_FTW.jpg'
+WHERE product_name LIKE '%EVGA FTW%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_Aero.png'
+WHERE product_name LIKE '%Gigabyte AERO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_Aorus.png'
+WHERE product_name LIKE '%Gigabyte AORUS%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_Aorus_Xtreme.png'
+WHERE product_name LIKE '%Gigabyte AORUS XTREME%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_Eagle_OC.png'
+WHERE product_name LIKE '%Gigabyte EAGLE OC%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_GeForce_RTX.png'
+WHERE product_name LIKE '%Gigabyte GAMING OC%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_GV.png'
+WHERE product_name LIKE '%Gigabyte GV%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Gigabyte_Windforce.png'
+WHERE product_name LIKE '%Gigabyte WINDFORCE%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Armor.png'
+WHERE product_name LIKE '%MSI ARMOR%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Gaming_Trio.png'
+WHERE product_name LIKE '%MSI GAMING TRIO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Gaming_RTX_5060_White.jpg'
+WHERE product_name LIKE '%MSI GAMING TRIO OC WHITE GeForce RTX 5070%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Gaming_X.png'
+WHERE product_name LIKE '%MSI GAMING X%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Gaming_X_Trio.png'
+WHERE product_name LIKE '%MSI GAMING X TRIO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Gaming_Z_Trio.png'
+WHERE product_name LIKE '%MSI GAMING Z TRIO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_GeForce_RTX.png'
+WHERE product_name LIKE '%MSI GeForce RTX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Vanguard.png'
+WHERE product_name LIKE '%MSI VANGUARD%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Ventus.png'
+WHERE product_name LIKE '%MSI VENTUS%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/MSI_Suprim.png'
+WHERE product_name LIKE '%MSI SUPRIM%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Nvidia_Founders.jpg'
+WHERE product_name LIKE '%NVIDIA Founders%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/PNY_XLR8.png'
+WHERE product_name LIKE '%PNY XLR8%'
+
+UPDATE Products
+SET image_url = './assets/img/products/GPU/Zotac_Gaming_RTX.jpg'
+WHERE product_name LIKE '%Zotac GAMING%'
