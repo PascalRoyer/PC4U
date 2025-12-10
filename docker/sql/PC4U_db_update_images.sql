c093cc7b2dde9df1dcd3121baf45f97152ea7902
Daniel <sekd01@uqo.ca>
2025-12-09 19:04:05 -0500

    Ajout d'images v2
    Corrections

--- docker/sql/PC4U_db_update_images.sql
+++ docker/sql/PC4U_db_update_images.sql
@@ -128,27 +128,27 @@ WHERE product_name LIKE '%Asus GT%'
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_STRIX_GAMING.webp'
-WHERE product_name LIKE '%Asus STRIX GAMING%'
+WHERE product_name LIKE '%Asus STRIX GAMING%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_STRIX_GAMING.webp'
-WHERE product_name LIKE '%Asus STRIX GAMING%'
+WHERE product_name LIKE '%Asus STRIX GAMING%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_ROG_STRIX_GAMING.jpg'
-WHERE product_name LIKE '%Asus ROG STRIX GAMING%'
+WHERE product_name LIKE '%Asus ROG STRIX GAMING%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_ROG_STRIX_WHITE.png'
-WHERE product_name LIKE '%Asus ROG STRIX WHITE OC GeForce RTX 3080 10GB%'
+WHERE product_name LIKE '%Asus ROG STRIX WHITE OC GeForce RTX 3080 10GB%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_TUF_Gaming.png'
-WHERE product_name LIKE '%Asus TUF GAMING GeForce RTX 4%'
+WHERE product_name LIKE '%Asus TUF GAMING GeForce RTX 4%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Asus_TUF_Gaming_OC.png'
-WHERE product_name LIKE '%Asus TUF GAMING OC%'
+WHERE product_name LIKE '%Asus TUF GAMING OC%' AND category_id = 2
 
 UPDATE Products
 SET image_url = './assets/img/products/GPU/EVGA_0.jpg'
@@ -245,3 +245,407 @@ WHERE product_name LIKE '%PNY XLR8%'
 UPDATE Products
 SET image_url = './assets/img/products/GPU/Zotac_Gaming_RTX.jpg'
 WHERE product_name LIKE '%Zotac GAMING%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_c049dq.png'
+WHERE product_name = 'Gigabyte AORUS CO49DQ'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_fi32q.png'
+WHERE product_id = 2307
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_fO27q2.png'
+WHERE product_id = 2308
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_fO27q3.png'
+WHERE product_id = 2309
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_fO32u2.png'
+WHERE product_id = 2310
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_aorus_fO32u2p.png'
+WHERE product_id = 2311
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Acer_Nitro.jpg'
+WHERE product_name LIKE '%Acer Nitro%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Acer_B276hul_Cymiippprx.webp'
+WHERE product_name = 'Acer B276HUL Cymiippprzx'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Acer_B276hul_Cymiippprx.webp'
+WHERE product_name = 'Acer B276HUL Cymiippprzx'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Acer_CB273U.jpg'
+WHERE product_name = 'Acer CB273U BEMIPRUZX'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/AOC_27G4XE.webp'
+WHERE product_name = 'AOC 27G4XE'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Acer_SB230.webp'
+WHERE product_name = 'Acer SB230 Bbix'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/LG_34GP950-B.jpg'
+WHERE product_name = 'LG UltraGear 34GP950G-B'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/MSI_Pro_MP251.png'
+WHERE product_name = 'MSI PRO MP251 E2'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Asus_ProArt_P427DCE.jpg'
+WHERE product_name = 'Asus ProArt Display PA27DCE-K'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Gigabyte_Gaming_GS27Q4.png'
+WHERE product_name = 'Gigabyte GS27QA'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Corsair_Xeneon.webp'
+WHERE product_name LIKE '%Corsair Xeneon%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Viewsonic_VX3211-4K-mhd_F01_pc.png'
+WHERE product_name LIKE '%Viewsonic%' AND product_name LIKE '%4K%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Lenovo_G.webp'
+WHERE product_name LIKE '%Lenovo G%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Planar_PL.jpg'
+WHERE product_name LIKE '%Planar PL%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Planar_PX.jpg'
+WHERE product_name LIKE '%Planar PX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Planar_PZ.jpg'
+WHERE product_name LIKE '%Planar PX248%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Monoprice_dark_matter.png'
+WHERE product_name = 'Monoprice Dark Matter'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Dell_Alienware_Aw.avif'
+WHERE product_name LIKE '%Dell Alienware AW%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Dell_Eseries.jpg'
+WHERE product_name LIKE '%Dell E%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Dell_Pseries.avif'
+WHERE product_name LIKE '%Dell P%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Asus_Rog_Strix.png'
+WHERE product_name LIKE '%Asus ROG Strix%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Asus_Rog_Swift.png'
+WHERE product_name LIKE '%Asus ROG Swift%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Asus_Rog_Swift_Oled.png'
+WHERE product_name LIKE '%Asus ROG Swift OLED%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Asus_TUF_Gaming.png'
+WHERE product_name LIKE '%Asus TUF Gaming%' AND product_name LIKE '%Asus TUF GAMING%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/LG_27GN.avif'
+WHERE product_name LIKE '%LG 27G%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/MSI_MAG_27.png'
+WHERE product_name LIKE '%MSI MAG 27%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/MSI_G2.png'
+WHERE product_name LIKE '%MSI G2%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/MSI_Optix.png'
+WHERE product_name LIKE '%MSI Optix%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Samsung_Odyssey_G3.webp'
+WHERE product_name LIKE '%Samsung Odyssey G3%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Monitors/Samsung_Odyssey_G5.avif'
+WHERE product_name LIKE '%Samsung Odyssey G5%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/ADATA_XPG_CASTER_RBG.jpg'
+WHERE product_name LIKE '%ADATA XPG CASTER RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/ADATA_XPG_GAMMIX.jpg'
+WHERE product_name LIKE '%ADATA XPG GAMMIX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Adata_xpg_lancer_blade.jpg'
+WHERE product_name LIKE '%ADATA XPG Lancer Blade%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Adata_xpg_lancer_rgb.png'
+WHERE product_name LIKE '%ADATA XPG LANCER%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_cmso.avif'
+WHERE product_name LIKE '%Corsair CMSO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_cmso.avif'
+WHERE product_name LIKE '%Corsair CMSO%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_value.avif'
+WHERE product_name LIKE '%Corsair CMV%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_dominator_platinum.avif'
+WHERE product_name LIKE '%Corsair Dominator Platinum%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_dominator_platinum_rgb.avif'
+WHERE product_name LIKE '%Corsair Dominator Platinum RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_dominator_titanium_rgb.avif'
+WHERE product_name LIKE '%Corsair Dominator Titanium%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_vengeance.avif'
+WHERE product_name LIKE '%Corsair Vengeance%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_vengeance_lpx.avif'
+WHERE product_name LIKE '%Corsair Vengeance LPX%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_vengeance_rgb.avif'
+WHERE product_name LIKE '%Corsair Vengeance RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_vengeance_rgb_pro.avif'
+WHERE product_name LIKE '%Corsair Vengeance RGB Pro%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Corsair_vengeance_rgb_pro_sl.avif'
+WHERE product_name LIKE '%Corsair Vengeance RGB Pro SL%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Crucial CT2K.jpeg'
+WHERE product_name LIKE '%Crucial CT2K%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Kingston_fury_renegade_rgb.jpg'
+WHERE product_name LIKE '%Kingston Fury Renegade RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Kingston_fury_renegade_pro.jpg'
+WHERE product_name LIKE '%Kingston FURY Renegade Pro%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Kingston_fury_beast.jpg'
+WHERE product_name LIKE '%Kingston FURY Beast%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Gskill_trident_z5_rgb.jpg'
+WHERE product_name LIKE '%G.Skill Trident Z5 RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Gskill_trident_z5_neo_rgb.jpg'
+WHERE product_name LIKE '%G.Skill Trident Z5 Neo RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Kingston_KVR.jpg'
+WHERE product_name LIKE '%Kingston KVR%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Mushkin_essentials.jpg'
+WHERE product_name LIKE '%Mushkin Essentials%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Mushkin_redline_lumina.jpg'
+WHERE product_name LIKE '%Mushkin Redline Lumina%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Patriot_signature_line.jpg'
+WHERE product_name LIKE '%Patriot Signature Line%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Memory/Patriot_viper_venom_rgb.jpg'
+WHERE product_name LIKE '%Patriot Viper Venom RGB%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Apricorn_Aegis_fortress.webp'
+WHERE product_name LIKE '%Apricorn Aegis Fortress%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Apricorn_Aegis_fortress_l3.jpg'
+WHERE product_name LIKE '%Apricorn Aegis Fortress L3%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Apricorn_Aegis_padlock.jpg'
+WHERE product_name LIKE '%Apricorn Aegis Padlock%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Apricorn_Aegis_padlock_ssd.jpg'
+WHERE product_name LIKE '%Apricorn Padlock SSD%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Apricorn_Aegis_padlock_ssd.jpg'
+WHERE product_name LIKE '%Apricorn Padlock SSD%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Crucial_x10.jpg'
+WHERE product_name LIKE '%Crucial X10%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Crucial_x.jpg'
+WHERE product_name LIKE '%Crucial X9%' /* faire x6 x8 et x9 */
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Sabrent_rocket_nano.jpg'
+WHERE product_name LIKE '%Sabrent Rocket Nano%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Samsung_t3_portable.webp'
+WHERE product_name LIKE '%Samsung T3%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Samsung_t5_portable.avif'
+WHERE product_name LIKE '%Samsung T5%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Samsung_t7_portable.webp'
+WHERE product_name LIKE '%Samsung T7 Portable%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Samsung_t5_portable.avif'
+WHERE product_name LIKE '%Samsung T7 Shield%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Seagate_backup.png'
+WHERE product_name LIKE '%Seagate Backup%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Seagate_onetouch.png'
+WHERE product_name LIKE '%Seagate One Touch%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/Western_digital_easystore.png'
+WHERE product_name LIKE '%Western Digital easystore%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/WD_elements.png'
+WHERE product_name LIKE '%Western Digital ELMENTS%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/WD_mybook.png'
+WHERE product_name LIKE '%Western Digital My Book Duo%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/WD_mypassport.png'
+WHERE product_name LIKE '%Western Digital My Passport%'
+
+UPDATE Products
+SET image_url = './assets/img/products/Storage/WD_Black.png'
+WHERE product_name LIKE '%Western Digital WD%'
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_ROG_STRIX_LCRTX3060.png'
+WHERE product_id = 751
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_tuf_Gaming_rtx3080ti.png'
+WHERE product_id = 778
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus TUF GAMING GeForce RTX.png'
+WHERE product_id = 784
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus TUF GAMING GeForce RTX.png'
+WHERE product_id = 785
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus TUF GAMING GeForce RTX.png'
+WHERE product_id = 786
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_TUF_Gaming_OC.png'
+WHERE product_id = 775
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_TUF_Gaming_OC.png'
+WHERE product_id = 776
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_TUF_Gaming_OC.png'
+WHERE product_id = 777
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_ROG_STRIX_WHITE.png'
+WHERE product_id = 757
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX-GTX1070-O8G-GAMING GeForce GTX 1070.webp'
+WHERE product_id = 758
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX Radeon RX VEGA 64.jpg'
+WHERE product_id = 755
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX Radeon RX VEGA 64.jpg'
+WHERE product_id = 754
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX Radeon RX VEGA 64.jpg'
+WHERE product_id = 753
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX OC GeForce RTX 4060.webp'
+WHERE product_id = 752
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_ROG_STRIX.jpg'
+WHERE product_id = 749
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus_ROG_STRIX.jpg'
+WHERE product_id = 750
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Asus ROG STRIX BTF GAMING OC GeForce RTX 4090.webp'
+WHERE product_id = 722
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Acer_nitro_gpu.jpg'
+WHERE product_id = 544
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Acer_nitro_gpu.jpg'
+WHERE product_id = 546
+
+UPDATE Products
+set image_url = './assets/img/products/GPU/Acer_nitro_gpu.jpg'
+WHERE product_id = 547
